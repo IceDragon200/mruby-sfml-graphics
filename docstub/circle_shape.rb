d54ab85ffbe1f83529b0313bd063d7ec4d88fba2
Corey Powell <mistdragon100@gmail.com>
2015-04-19 13:02:03 -0500

    Completed CircleShape

---
+++ docstub/circle_shape.rb
@@ -0,0 +1,19 @@
+module SFML
+  # A shape for rendering Circles
+  class CircleShape < Shape
+    def initialize
+    end
+
+    # @param [Float] rad
+    def set_radius(rad)
+    end
+
+    # @param [Integer] count
+    def set_point_count(count)
+    end
+
+    alias :radius= :set_radius
+    alias :radius :get_radius
+    alias :point_count= :set_point_count
+  end
+end
--- src/mrb_circle_shape.cxx
+++ src/mrb_circle_shape.cxx
@@ -28,8 +28,46 @@ get_circle_shape(mrb_state *mrb, mrb_value self)
 static mrb_value
 circle_shape_initialize(mrb_state *mrb, mrb_value self)
 {
-  sf::CircleShape *circle_shape = new sf::CircleShape();
+  mrb_float radius;
+  mrb_int point_count;
+  mrb_int argc = mrb_get_args(mrb, "|fi", &radius, &point_count);
+  sf::CircleShape *circle_shape;
+  if (argc == 0) {
+    circle_shape = new sf::CircleShape();
+  } else if (argc == 1) {
+    circle_shape = new sf::CircleShape(radius);
+  } else if (argc == 2) {
+    circle_shape = new sf::CircleShape(radius, point_count);
+  } else {
+    mrb_raise(mrb, E_ARGUMENT_ERROR, "expected 0, 1 or 2");
+    return self;
+  }
+  circle_shape_free(mrb, DATA_PTR(self));
   mrb_data_init(self, circle_shape, &mrb_sfml_circle_shape_type);
+  return self;
+}
+
+static mrb_value
+circle_shape_set_radius(mrb_state *mrb, mrb_value self)
+{
+  mrb_float radius;
+  mrb_get_args(mrb, "f", &radius);
+  get_circle_shape(mrb, self)->setRadius(radius);
+  return self;
+}
+
+static mrb_value
+circle_shape_get_radius(mrb_state *mrb, mrb_value self)
+{
+  return mrb_float_value(mrb, get_circle_shape(mrb, self)->getRadius());
+}
+
+static mrb_value
+circle_shape_set_point_count(mrb_state *mrb, mrb_value self)
+{
+  mrb_int points;
+  mrb_get_args(mrb, "i", &points);
+  get_circle_shape(mrb, self)->setPointCount(points);
   return self;
 }
 
@@ -39,6 +77,13 @@ mrb_sfml_circle_shape_init_bind(mrb_state *mrb, struct RClass *mod)
   circle_shape_class = mrb_define_class_under(mrb, mod, "CircleShape", mrb_class_get_under(mrb, mod, "Shape"));
   MRB_SET_INSTANCE_TT(circle_shape_class, MRB_TT_DATA);
 
-  mrb_define_method(mrb, circle_shape_class, "initialize", circle_shape_initialize, MRB_ARGS_NONE());
+  mrb_define_method(mrb, circle_shape_class, "initialize", circle_shape_initialize, MRB_ARGS_ANY());
   shape_bind_class<sf::CircleShape>(mrb, circle_shape_class);
+  mrb_define_method(mrb, circle_shape_class, "set_radius",      circle_shape_set_radius, MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, circle_shape_class, "get_radius",      circle_shape_get_radius, MRB_ARGS_NONE());
+  mrb_define_method(mrb, circle_shape_class, "set_point_count", circle_shape_set_point_count, MRB_ARGS_REQ(1));
+
+  mrb_define_alias(mrb, circle_shape_class, "radius=", "set_radius");
+  mrb_define_alias(mrb, circle_shape_class, "radius", "get_radius");
+  mrb_define_alias(mrb, circle_shape_class, "point_count=", "set_point_count");
 }
