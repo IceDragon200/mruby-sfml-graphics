9b0c092d184e9c0435fad913182ac31614d5f753
Corey Powell <mistdragon100@gmail.com>
2015-05-02 10:44:29 -0500

    Stubbed Font

---
+++ src/mrb_font.cxx
@@ -0,0 +1,18 @@
+#include <mruby.h>
+#include <mruby/class.h>
+#include <mruby/data.h>
+#include <SFML/Graphics/Font.hpp>
+#include "mrb/cxx/helpers.hxx"
+#include "mrb/sfml/graphics/font.hxx"
+
+struct RClass *font_class;
+static mrb_data_free_func font_free = cxx_mrb_data_free<sf::Font>;
+
+extern "C" const mrb_data_type mrb_sfml_font_type = { "sf::Font", font_free };
+
+extern "C" void
+mrb_sfml_font_init_bind(mrb_state *mrb, struct RClass *mod)
+{
+  font_class = mrb_define_class_under(mrb, mod, "Font", mrb->object_class);
+  MRB_SET_INSTANCE_TT(font_class, MRB_TT_DATA);
+}
---
+++ src/mrb_font.hxx
@@ -0,0 +1,9 @@
+#ifndef MRB_SFML_FONT_PRIV_H
+#define MRB_SFML_FONT_PRIV_H
+
+#include <mruby.h>
+#include <mruby/class.h>
+
+extern "C" void mrb_sfml_font_init_bind(mrb_state *mrb, struct RClass *mod);
+
+#endif
