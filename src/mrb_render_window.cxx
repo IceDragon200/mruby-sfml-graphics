8d3dea0973e48451eca6bdf6258d8a546d323034
Corey Powell <mistdragon100@gmail.com>
2015-05-02 10:44:43 -0500

    Started RenderWindow

---
+++ src/mrb_render_window.cxx
@@ -0,0 +1,33 @@
+#include <mruby.h>
+#include <mruby/class.h>
+#include <mruby/data.h>
+#include <mruby/numeric.h>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include "mrb/cxx/helpers.hxx"
+#include "mrb/sfml/window/window.hxx"
+#include "mrb/sfml/window/window_bind.hxx"
+#include "mrb/sfml/graphics/render_target_bind.hxx"
+#include "mrb/sfml/graphics/render_window.hxx"
+#include "mrb/sfml/graphics/image.hxx"
+#include "mrb/sfml/system/vector2.hxx"
+#include "mrb_render_window.hxx"
+
+static mrb_data_free_func render_window_free = cxx_mrb_data_free<sf::RenderWindow>;
+extern "C" const struct mrb_data_type mrb_sfml_render_window_type = { "sf::RenderWindow", render_window_free };
+
+static mrb_value
+render_window_capture(mrb_state *mrb, mrb_value self)
+{
+  return mrb_sfml_image_value(mrb, mrb_sfml_render_window_ptr(mrb, self)->capture());
+}
+
+extern "C" void
+mrb_sfml_render_window_init_bind(mrb_state *mrb, struct RClass *mod)
+{
+  struct RClass *render_window_class = mrb_define_class_under(mrb, mod, "RenderWindow", mrb_class_get_under(mrb, mod, "Window"));
+  MRB_SET_INSTANCE_TT(render_window_class, MRB_TT_DATA);
+
+  mrb_sfml_window_bind_class<sf::RenderWindow>(mrb, render_window_class);
+  mrb_sfml_render_target_bind_class<sf::RenderWindow>(mrb, render_window_class);
+  mrb_define_method(mrb, render_window_class, "capture", render_window_capture, MRB_ARGS_NONE());
+}
---
+++ src/mrb_render_window.hxx
@@ -0,0 +1,9 @@
+#ifndef MRB_SFML_RENDER_WINDOW_PRIV_H
+#define MRB_SFML_RENDER_WINDOW_PRIV_H
+
+#include <mruby.h>
+#include <mruby/class.h>
+
+extern "C" void mrb_sfml_render_window_init_bind(mrb_state *mrb, struct RClass *mod);
+
+#endif
