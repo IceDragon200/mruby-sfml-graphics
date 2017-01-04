63164cf142ee387836033897b8e16a6ad4ef8a87
Corey Powell <mistdragon100@gmail.com>
2017-01-03 21:10:29 -0500

    Updated Texture binding to SFML 2.4.1

--- src/mrb_texture.cxx
+++ src/mrb_texture.cxx
@@ -11,7 +11,6 @@
 #include "mrb/sfml/system/vector2.hxx"
 #include "mrb_texture.hxx"
 
-static struct RClass *texture_class;
 static mrb_data_free_func texture_free = cxx_mrb_data_free<sf::Texture>;
 extern "C" const struct mrb_data_type mrb_sfml_texture_type = { "sf::Texture", texture_free };
 
@@ -130,25 +129,110 @@ texture_is_smooth(mrb_state *mrb, mrb_value self)
   return mrb_bool_value(mrb_sfml_texture_ptr(mrb, self)->isSmooth());
 }
 
+static mrb_value
+texture_set_srgb(mrb_state *mrb, mrb_value self)
+{
+  mrb_bool b;
+  mrb_get_args(mrb, "b", &b);
+  mrb_sfml_texture_ptr(mrb, self)->setSrgb(b);
+  return self;
+}
+
+static mrb_value
+texture_is_srgb(mrb_state *mrb, mrb_value self)
+{
+  return mrb_bool_value(mrb_sfml_texture_ptr(mrb, self)->isSrgb());
+}
+
+static mrb_value
+texture_set_repeated(mrb_state *mrb, mrb_value self)
+{
+  mrb_bool repeated;
+  mrb_get_args(mrb, "b", &repeated);
+  mrb_sfml_texture_ptr(mrb, self)->setRepeated(repeated);
+  return self;
+}
+
+static mrb_value
+texture_is_repeated(mrb_state *mrb, mrb_value self)
+{
+  return mrb_bool_value(mrb_sfml_texture_ptr(mrb, self)->isRepeated());
+}
+
+static mrb_value
+texture_generate_mipmap(mrb_state *mrb, mrb_value self)
+{
+  return mrb_bool_value(mrb_sfml_texture_ptr(mrb, self)->generateMipmap());
+}
+
+static mrb_value
+texture_get_native_handle(mrb_state *mrb, mrb_value self)
+{
+  return mrb_fixnum_value(mrb_sfml_texture_ptr(mrb, self)->getNativeHandle());
+}
+
+static mrb_value
+texture_s_bind(mrb_state *mrb, mrb_value self)
+{
+  sf::Texture *texture;
+  mrb_int coord_type = (mrb_int)sf::Texture::CoordinateType::Normalized;
+  mrb_get_args(mrb, "d|i", &texture, &mrb_sfml_texture_type, &coord_type);
+  sf::Texture::bind(texture, (sf::Texture::CoordinateType)coord_type);
+  return self;
+}
+
+static mrb_value
+texture_s_get_maximum_size(mrb_state *mrb, mrb_value self)
+{
+  return mrb_fixnum_value(sf::Texture::getMaximumSize());
+}
+
 extern "C" void
 mrb_sfml_texture_init_bind(mrb_state *mrb, struct RClass *mod)
 {
-  texture_class = mrb_define_class_under(mrb, mod, "Texture", mrb->object_class);
+  struct RClass *texture_class = mrb_define_class_under(mrb, mod, "Texture", mrb->object_class);
+  struct RClass *coordinate_type_mod = mrb_define_module_under(mrb, texture_class, "CoordinateType");
+  mrb_define_const(mrb, coordinate_type_mod, "Normalized", mrb_fixnum_value(sf::Texture::CoordinateType::Normalized));
+  mrb_define_const(mrb, coordinate_type_mod, "Pixels", mrb_fixnum_value(sf::Texture::CoordinateType::Pixels));
+
   MRB_SET_INSTANCE_TT(texture_class, MRB_TT_DATA);
 
   mrb_define_method(mrb, texture_class, "initialize",         texture_initialize,         MRB_ARGS_ARG(0,1));
   mrb_define_method(mrb, texture_class, "create",             texture_create,             MRB_ARGS_REQ(2));
   mrb_define_method(mrb, texture_class, "load_from_file",     texture_load_from_file,     MRB_ARGS_ARG(1,1));
   mrb_define_method(mrb, texture_class, "load_from_image",    texture_load_from_image,    MRB_ARGS_ARG(1,1));
+
   mrb_define_method(mrb, texture_class, "get_size",           texture_get_size,           MRB_ARGS_NONE());
+  mrb_define_alias(mrb, texture_class, "size", "get_size");
+
   mrb_define_method(mrb, texture_class, "copy_to_image",      texture_copy_to_image,      MRB_ARGS_NONE());
+  mrb_define_alias(mrb, texture_class, "to_image", "copy_to_image");
+
   mrb_define_method(mrb, texture_class, "update_from_image",  texture_update_from_image,  MRB_ARGS_ARG(1,2));
   mrb_define_method(mrb, texture_class, "update_from_window", texture_update_from_window, MRB_ARGS_ARG(1,2));
   mrb_define_method(mrb, texture_class, "set_smooth",         texture_set_smooth,         MRB_ARGS_REQ(1));
   mrb_define_method(mrb, texture_class, "is_smooth",          texture_is_smooth,          MRB_ARGS_NONE());
-
-  mrb_define_alias(mrb, texture_class, "size", "get_size");
-  mrb_define_alias(mrb, texture_class, "to_image", "copy_to_image");
   mrb_define_alias(mrb, texture_class, "smooth=", "set_smooth");
+  mrb_define_alias(mrb, texture_class, "smooth", "is_smooth");
   mrb_define_alias(mrb, texture_class, "smooth?", "is_smooth");
+
+  mrb_define_method(mrb, texture_class, "set_srgb",           texture_set_srgb,           MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, texture_class, "is_srgb",            texture_is_srgb,            MRB_ARGS_NONE());
+  mrb_define_alias(mrb, texture_class, "srgb=", "set_srgb");
+  mrb_define_alias(mrb, texture_class, "srgb", "is_srgb");
+  mrb_define_alias(mrb, texture_class, "srgb?", "is_srgb");
+
+  mrb_define_method(mrb, texture_class, "set_repeated",       texture_set_repeated,       MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, texture_class, "is_repeated",        texture_is_repeated,        MRB_ARGS_NONE());
+  mrb_define_alias(mrb, texture_class, "repeated=", "set_repeated");
+  mrb_define_alias(mrb, texture_class, "repeated", "is_repeated?");
+  mrb_define_alias(mrb, texture_class, "repeated?", "is_repeated?");
+
+  mrb_define_method(mrb, texture_class, "generate_mipmap", texture_generate_mipmap, MRB_ARGS_NONE());
+
+  mrb_define_method(mrb, texture_class, "get_native_handle",  texture_get_native_handle,  MRB_ARGS_NONE());
+  mrb_define_alias(mrb, texture_class, "native_handle", "get_native_handle");
+
+  mrb_define_class_method(mrb, texture_class, "bind",             texture_s_bind,             MRB_ARGS_ARG(1,1));
+  mrb_define_class_method(mrb, texture_class, "get_maximum_size", texture_s_get_maximum_size, MRB_ARGS_NONE());
 }
